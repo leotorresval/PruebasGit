db1df27eab75dadf11b694932ce17cda4d0b27bc
leotorresval <leotorresval92@gmail.com>
2023-01-17 22:10:19 -0500

    Primer commit

---
+++ src/main/java/pruebas/git/PruebasGitApplication.java
@@ -0,0 +1,13 @@
+package pruebas.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebasGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebasGitApplication.class, args);
+	}
+
+}
